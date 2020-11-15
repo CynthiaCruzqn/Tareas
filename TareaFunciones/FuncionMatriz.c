a4d2d9b5e5e37865b170763611327fa1d23cea21
CynthiaCruzqn <cynthia.cruzqn@uanl.edu.mx>
2020-11-15 16:16:02 -0600

    correccion de la funcion de suma de matrices

--- TareaFunciones/FuncionMatriz.c
+++ TareaFunciones/FuncionMatriz.c
@@ -1,52 +1,66 @@
 #include<stdio.h>
 
+const int MAX = 50;
 
 
-int main(){
+void funcion_lectura_de_matriz(int [][MAX], int, int);
+void funcion_suma_de_matriz(int [][MAX], int [][MAX], int [][MAX], int, int);
+void funcion_impresion_de_matriz(int [][MAX], int, int);
 
-  int dimension = 5;
-  int Matriz1 [dimension][dimension], Matriz2 [dimension][dimension];
-  int MatrizResult [dimension][dimension];
-  int i, j;
 
-  for (j = 0; j < dimension; j ++)
-     {
-       for(i = 0; i < dimension; i ++)
-          {
-            Matriz1 [j][i] = i + j;
-            Matriz2 [j][i] = i + j + 100;
-            MatrizResult[j][i] =
-            Matriz1 [j][i] + Matriz2 [j][i];
+int main(int num_de_arg, char **dimension){
+  int Matriz1[MAX][MAX], Matriz2[MAX][MAX], MatrizResult[MAX][MAX];
+  int filas, columnas, contador;
 
-          }
-     }
-  printf("La Primera matriz es: \n" );
-  for (j = 0; j < dimension; j ++)
-     {
-       for (i = 0; i < dimension; i ++)
-          {
-            printf("%d, ", Matriz1[j][i]);
-          }
-       printf("\n");
-     }
-     printf("La Segunda matriz es: \n" );
-     for (j = 0; j < dimension; j ++)
-        {
-          for (i = 0; i < dimension; i ++)
-             {
-               printf("%d, ", Matriz2[j][i]);
-             }
-          printf("\n");
-        }
-        printf("La matriz resultante es: \n" );
-        for (j = 0; j < dimension; j ++)
-           {
-             for (i = 0; i < dimension; i ++)
-                {
-                  printf("%d, ", MatrizResult[j][i]);
+do{
+  printf("\nIngrese el numero de filas de las matrices: ");
+  scanf("%d", &filas);
+}while(filas > MAX || filas < 1);
+
+do {
+  printf("\nIngrese el numero de columnas de las matrices: ");
+  scanf("%d", &columnas);
+} while(columnas > MAX || columnas < 1);
+
+printf("\nLectura de la primer matriz\n");
+funcion_lectura_de_matriz(Matriz1, filas, columnas);
+printf("\nLectura de la segunda matriz\n");
+funcion_lectura_de_matriz(Matriz2, filas, columnas);
+funcion_suma_de_matriz(Matriz1, Matriz2, MatrizResult, filas, columnas);
+printf("\n\n La Matriz 1 es:\n\n" );
+funcion_impresion_de_matriz(Matriz1, filas, columnas);
+printf("\n\n La Matriz 2 es:\n\n" );
+funcion_impresion_de_matriz(Matriz2, filas, columnas);
+printf("\n\nMatriz resultante de la sumatoria:\n\n" );
+funcion_impresion_de_matriz(MatrizResult, filas, columnas);
+
+}
+
+void funcion_lectura_de_matriz(int A[][MAX], int f, int c){
+        int i, j;
+        for(i = 0; i < f; i++){
+                for(j = 0; j < c; j ++){
+                        printf("Ingrese el elemento de%d %d: ", i + 1, j + 1);
+                        scanf("%d", &A[i][j]);
                 }
-             printf("\n");
-           }
+        }
+}
 
-           return 0;
+void funcion_suma_de_matriz(int Matriz1[][MAX], int Matriz2[][MAX], int Matriz3[][MAX], int f, int c){
+        int i, j;
+        for(i = 0; i < f; i ++){
+                for(j = 0; j < c; j ++){
+                        Matriz3[i][j] = Matriz1[i][j] + Matriz2[i][j];
+                }
+        }
+}
+
+void funcion_impresion_de_matriz(int A[][MAX], int f, int c){
+        int i, j;
+        for(i = 0; i < f; i ++){
+                for(j = 0; j < c; j ++){
+                        printf("\t%d", A[i][j]);
+                }
+                printf("\n");
+        }
 }
