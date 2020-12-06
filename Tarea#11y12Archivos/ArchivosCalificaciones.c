a3d85e66bf8dabc9a2ad2db29f02caceb600af2c
CynthiaCruzqn <cynthia.cruzqn@uanl.edu.mx>
2020-12-05 19:53:53 -0600

    correcion de errores en almacenamiento de inf

--- Tarea#11y12Archivos/ArchivosCalificaciones.c
+++ Tarea#11y12Archivos/ArchivosCalificaciones.c
@@ -9,7 +9,6 @@ typedef struct Calificaciones{
 }calif;
 
 
-
 int main(int argc, char *argv[]){
 
 	calif resultados;
@@ -20,13 +19,12 @@ int main(int argc, char *argv[]){
 	resultados.final_str = (resultados.Parcial_uno + resultados.Parcial_dos + resultados.PIA)/3;
 
 
-
-  FILE *fp;
-  fp = fopen("Calificaciones.csv", "a");
-  int i = 1;
-   fprintf(fp, "Matricula, PrimerParcial, SegundoParcial, PIA, Final\n");
+	FILE *fp;
+	fp = fopen("Calificaciones.csv", "a");
+	int i = 1;
+   //fprintf(fp, "Matricula, PrimerParcial, SegundoParcial, PIA, Final\n");
    fprintf(fp, "%d, %d, %d, %d, %.2f\n", resultados.Matricula, resultados.Parcial_uno, resultados.Parcial_dos, resultados.PIA, resultados.final_str);
-       printf("Calificaciones de alumno %d son: %d %d %d %d %.2f\n", i ++, resultados.Matricula, resultados.Parcial_uno, resultados.Parcial_dos, resultados.PIA, resultados.final_str);
+       printf("Calificaciones de alumno %d son: %d %d %d %.2f\n", resultados.Matricula, resultados.Parcial_uno, resultados.Parcial_dos, resultados.PIA, resultados.final_str);
   fflush(fp);
   fclose(fp);
 
